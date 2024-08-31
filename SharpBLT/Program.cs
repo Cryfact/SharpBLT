d49c03f81fda688d75e95136e6a742440c3de824
BangL <henno.rickowski@gmail.com>
2024-08-31 19:46:25 +0200

    fix directory check

--- SharpBLT/Program.cs
+++ SharpBLT/Program.cs
@@ -6,13 +6,13 @@ namespace SharpBLT
     {
         private static void ValidateModDirectories()
         {
-            if (Directory.Exists("mods/downloads"))
+            if (!Directory.Exists("mods/downloads"))
                 Directory.CreateDirectory("mods/downloads");
 
-            if (Directory.Exists("mods/logs"))
+            if (!Directory.Exists("mods/logs"))
                 Directory.CreateDirectory("mods/logs");
 
-            if (Directory.Exists("mods/saves"))
+            if (!Directory.Exists("mods/saves"))
                 Directory.CreateDirectory("mods/saves");
 
         }
