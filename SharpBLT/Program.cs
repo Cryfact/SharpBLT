7606848496b6779feb7d449bb158abd5edf9f4a8
Cryfact <git@cryfact.de>
2024-08-31 03:37:16 +0200

    entrypoint fixed

--- SharpBLT/Program.cs
+++ SharpBLT/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +22,7 @@ namespace SharpBLT
 
         }
 
+        [UnmanagedCallersOnly]
         public static void Main()
         {
             ValidateModDirectories();
